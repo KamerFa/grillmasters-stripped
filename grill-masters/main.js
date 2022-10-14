2da7c7738cf7e9c742b3fc4b328b45e84bb5179c
KamerFa <108697394+KamerFa@users.noreply.github.com>
2022-10-14 03:19:44 +0200

    js cleanup

--- grill-masters/main.js
+++ grill-masters/main.js
@@ -18,30 +18,3 @@ whatsButtons.forEach(button => {
     })
 
 })
-
-const slider = document.querySelector('.slidertest');
-let isDown = false;
-let startX;
-let sLeft;
-slider.scrollLeft = 0;
-
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX;
-  sLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-});
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-});
-
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX;
-  const dragged = x - startX;
-  slider.scrollLeft = sLeft - dragged;
-}); 
